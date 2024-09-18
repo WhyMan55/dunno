2edd2f215eebdec77abe0f95a3a4650c170b5799
Антон Ушаков <bruuuhv123456@gmail.com>
2024-09-18 20:20:35 +0300

    Add files via upload

---
+++ idk.c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+
+void main()	
+{
+	printf("var9\n");
+	float n, L;
+	n = 4.0;
+	L = 393.0;
+	printf("Dano:\n        %f\n      %f\n   -------------\nOtvet:\n     %+010.6f",  n, L, n/L);
+}
